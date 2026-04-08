cbd82b5f9633943b8f51499dfc32856509e9e262
AlexanderSc21 <alexandersinte21@gmail.com>
2026-04-07 19:48:51 -0500

    Primer commit - proyecto Angular base

---
+++ Backend/GestionFarmacia/src/main/java/com/proyecto/GestionFarmacia/GestionFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.GestionFarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFarmaciaApplication.class, args);
+	}
+
+}
